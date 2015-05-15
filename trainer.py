fc69ffabc206a530f706471c91e973b2da57f43e
cdgore <cdgore@cdgore.com>
2015-05-15 13:56:53 -0700

    Added function to estimate the intercept on data stored as spark rdd

--- trainer.py
+++ trainer.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy as sp
-import scipy.sparse
+import scipy.sparse as sparse
 # import pandas as pd
 import itertools
 # import math
@@ -12,7 +12,7 @@ from preprocess.feature_hash import parse_row
 
 k = 18
 # Initialize weight vector
-w = sp.sparse.csc_matrix((1 << k, 1), dtype=np.float)
+w = sparse.csc_matrix((1 << k, 1), dtype=np.float)
 alpha = 0.1
 
 # Regularization parameter
@@ -57,6 +57,18 @@ def set_first_intercept(w, X1):
         map(
             lambda x: (x[0], 1),
             X1))
+    ctr_ratio = float(ctr_counts[0]) / float(ctr_counts[1]) + 10**-6
+    new_intercept = -np.log(ctr_ratio**-1 - 1)
+    w[0, 0] = new_intercept
+    return w
+
+
+def set_first_intercept_spark(w, X1):
+    ctr_counts = X1.map(
+        lambda x: (x[0], 1)
+    ).reduce(
+        lambda a, b: (a[0] + b[0], a[1] + b[1])
+    )
     ctr_ratio = float(ctr_counts[0]) / float(ctr_counts[1]) + 10**-6
     new_intercept = -np.log(ctr_ratio**-1 - 1)
     w[0, 0] = new_intercept
