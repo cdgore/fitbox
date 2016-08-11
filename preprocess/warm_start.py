5acfd146b047e9849e844efb83e9a44cc80bbfec
cdgore <cdgore@cdgore.com>
2016-08-11 16:41:50 -0700

    refactor set_first_intercept for spark for arbitrary sized tensors

--- preprocess/warm_start.py
+++ preprocess/warm_start.py
@@ -21,7 +21,15 @@ def set_first_intercept_spark(w, X1):
         (0, 0),
         lambda a, b: (a[0] + b[0], a[1] + b[1])
     )
-    ctr_ratio = float(ctr_counts[0]) / float(ctr_counts[1]) + 10**-6
-    new_intercept = -np.log(ctr_ratio**-1 - 1)
-    w[0, 0] = new_intercept
+    numer = ctr_counts[0]
+    denom = ctr_counts[1]
+    if denom == 0:
+        denom = 1
+    ctr_ratio = numer / denom
+    # new_intercept = -ln(ctr_ratio**-1 - 1)
+    ctr_ratio.data = -np.log([
+        ((_ + 10**-6) ** -1) - 1.
+        for _ in ctr_ratio.data])
+    new_intercept = ctr_ratio
+    w[0] = new_intercept.T
     return w
